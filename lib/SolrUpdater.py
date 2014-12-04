6b40ae8dbeb52b4682c2c1021bca2f15542dec1b
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-12-04 14:02:03 -0500

    SolrUpdater: Concat multiple comments with newline instead of picking the first

--- lib/SolrUpdater.py
+++ lib/SolrUpdater.py
@@ -269,7 +269,7 @@ class SolrAdapter(object):
   def _comment(ADS_record):
     result = [i['content'] for i in ADS_record['metadata']['general'].get('comment',[])]
     if len(result)>1: #Hack to avoid a re-indexing because of non-multivalued field 'comment'
-      result = result[0] 
+      result = '\n'.join(result)
     return {'comment': result}
 
   @staticmethod
