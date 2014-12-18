cb0acbdef388059ddf98ee6277b44860c02a87ac
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-12-18 05:20:21 -0500

    workers: typo

--- pipeline/workers.py
+++ pipeline/workers.py
@@ -82,7 +82,7 @@ class ErrorHandlerWorker(RabbitMQWorker):
       'UpdateRecordsWorker':    UpdateRecords.mergeRecords, #expects [{record}, ...]
       'MongoWriteWorker':       self.mongo.upsertRecords, #expects [{records}, ...]
       'SolrUpdateWorker':       SolrUpdater.solrUpdate, #expects ['bibcode', ...]
-      'FindDeletedRecordsWorker':   lambda f: return [], #Since this job __requires__ the entire dataset, we cannot split it
+      'FindDeletedRecordsWorker':   lambda f: [], #Since this job __requires__ the entire dataset, we cannot split it
       'DeletionWorker':         self.mongo.remove, #expects ['bibcode',...]
     }
 
@@ -234,7 +234,7 @@ class FindDeletedRecordsWorker(RabbitMQWorker):
       results = [i['bibcode'] for i in self.f(message,query_limiter={'bibcode':1,'_id':0})]
       results = list(set(message).difference(results))
       if results:
-        self.publish(json.dumps(results)
+        self.publish(json.dumps(results))
     except Exception, e:
       self.logger.error('%s: %s' % (e,traceback.format_exc()))
       self.logger.warning("Offloading to ErrorWorker due to exception: %s" % e)
